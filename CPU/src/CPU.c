12f24b9e42e33fbda839a16943df539c5791ebb8
jyp2712 <pompeypabl@est.frba.utn.edu.ar>
2017-04-03 00:34:20 -0300

    Agregué comentarios sobre funciones
    Agregué comentarios para entender los pasos de crear una conexión

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -7,10 +7,12 @@ int main(){
 
 	struct sockaddr_in direccionServidor;
 	direccionServidor.sin_family = AF_INET;
-	direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+	direccionServidor.sin_addr.s_addr = inet_addr("127.0.0.1"); /* IP a la que se tiene que conectar */
 	direccionServidor.sin_port = htons(8080);
 
 	int cliente = socket(AF_INET, SOCK_STREAM, 0);
+	
+	/* conect devuelve un N° distinto de 0 si falla */
 	if (connect(cliente, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
 		perror("No se pudo conectar");
 		return 1;
