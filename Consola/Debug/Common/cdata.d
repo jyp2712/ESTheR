8fa5e001ad38ece55b6314a587bd3b561a999290
nanorarias <nanorarias@gmail.com>
2017-04-13 14:21:20 -0300

    Los compilables *no* deberían haberse sincronizado. Removiendo.
    Estos archivos se generan cada vez que se compila el proyecto,
    por lo que no deberían estar en GitHub. Se sincronizaron por un
    error mío cuando edité el .gitignore. Ya debería estar arreglado.
    
    Entschuldigung.

--- Consola/Debug/Common/cdata.d
+++
@@ -1,1 +0,0 @@
-Common/cdata.d: /home/utnso/git/tp-2017-1c-System-Buzz/Common/cdata.c
--- File_System/Debug/Common/globals.d
+++
@@ -1,4 +0,0 @@
-Common/globals.d: /home/utnso/git/tp-2017-1c-System-Buzz/Common/globals.c \
- /home/utnso/git/tp-2017-1c-System-Buzz/Common/globals.h
-
-/home/utnso/git/tp-2017-1c-System-Buzz/Common/globals.h:
